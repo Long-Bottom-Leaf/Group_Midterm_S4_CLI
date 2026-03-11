0f2f575a582f0d07d3135d1dfb21300648e91ef2
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-03-10 21:40:00 -0230

    City model

--- src/main/java/com/example/airport_cli/model/City.java
+++ src/main/java/com/example/airport_cli/model/City.java
@@ -1,4 +1,72 @@
 package com.example.airport_cli.model;
 
+import java.util.List;
+
 public class City {
+
+    private Long id;
+    private String name;
+    private String province;
+    private int population;
+
+    private List<Airport> airports;
+
+    // empty constructor for json objects
+    public City() {}
+
+    public City(String name, String province) {
+        this.name = name;
+        this.province = province;
+        this.population = 0;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public List<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(List<Airport> airports) {
+        this.airports = airports;
+    }
+
+    @Override
+    public String toString() {
+        return "City ID: " + getId() +
+                "\nCity Name: " + getName() +
+                "\nProvince: " + getProvince() +
+                "\nPopulation: " + getPopulation() +
+                "\nAirports: " + getAirports();
+    }
 }
